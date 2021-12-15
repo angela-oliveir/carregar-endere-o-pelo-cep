5f4de577d2c48308b86575ec4caf816374f558fd
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2021-12-15 20:31:42 -0300

    Importando CEPs do arquivo csv
    Importei informações de endereço através de CEPs que estavam em arquivo csv

--- importando_arquivo_csv.py
+++ importando_arquivo_csv.py
@@ -1,10 +1,12 @@
 
 
-
+from pycep_correios import get_address_from_cep, WebService
 import csv
-
 with open('planilha-de-cep-pagina1.csv', encoding="utf8", errors='ignore') as arquivo:
     mostrar_arquivo = csv.reader(arquivo)
-    next(mostrar_arquivo)
-    for arquivo_csv in mostrar_arquivo:
-        print(arquivo_csv)
+    next(mostrar_arquivo)    
+    for cep in mostrar_arquivo:
+        print(cep[0])  
+        endereço_completo = get_address_from_cep(cep[0], webservice=WebService.CORREIOS)
+        print(endereço_completo)
+        
